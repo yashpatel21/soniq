b0a818cf1f7db3e7054e403bd694fcc202c15da0
Yash Patel <yash.patel0419@gmail.com>
2025-04-02 17:35:13 -0500

    Update version to 0.9.4, add transpilePackages option for 'moises' in next.config.ts, and include patch-package in postinstall script for dependency management.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	transpilePackages: ['moises'],
 }
 
 export default nextConfig
